6cda5e3ab4cf0b8a82677b46609508031abf355b
SiyuanPengMike <43769590+SiyuanPengMike@users.noreply.github.com>
2018-11-25 23:18:52 -0600

    Assign 7 finished

---
+++ Assignments/A7/Problem1(upload)/test_problem2.py
@@ -0,0 +1,10 @@
+# Then, write the test for this function.Save this test as test_problem2.py
+import problem2
+def test_problem2():
+    assert problem2.month_length("February", leap_year = False) == 28, 'failed on February in common year'
+    assert problem2.month_length("February", leap_year = True) == 29, 'failed on February in leap year'
+    assert problem2.month_length("March", leap_year = False) == 31, 'failed on March in common year'
+    assert problem2.month_length("March", leap_year = True) == 31, 'failed on March in leap year'
+    assert problem2.month_length("April", leap_year = False) == 30, 'failed on April in common year'
+    assert problem2.month_length("April", leap_year = True) == 30, 'failed on April in leap year'
+    assert problem2.month_length("MikePeng") == None, 'failed on strings other than monthes'
